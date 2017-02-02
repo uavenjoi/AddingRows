cda1056355a08c98cb7d1290457864ff28ec3e93
Mikhail Sokolov <karsat86@mail.ru>
2017-02-02 22:55:13 +0600

    review
     if (Double.TryParse(afterSeparStrings[0], out _value1))
                {
                    if (Double.TryParse(afterSeparStrings[1], out _value2))
                    {
    лучше сначала проинициализировать 2 переменные а потом в одном условии проверить if( x!=null && y != null). Вложенные условия запутывают программу.
    2 раза в разных местах обрабатыватеся текст, можно обойтись одним разом. 
    text.Split(new char[] {'+', '-', '*', '/', ' '},  
    text.Split(new char[] {' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},

--- Action.cs
+++ Action.cs
@@ -26,7 +26,7 @@ namespace AddingRows
             {
                 if (Double.TryParse(afterSeparStrings[1], out _value2))
                 {
-                    afterSeparStrings = text.Split(new char[] {' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},
+                    afterSeparStrings = text.Split(new char[] {' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}, 
                         StringSplitOptions.RemoveEmptyEntries);
                     if (afterSeparStrings.Length != 1)
                     {
